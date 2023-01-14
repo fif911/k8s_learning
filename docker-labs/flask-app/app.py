1d5e06083ff4c5335f063260239047ecdca2d8e6
Alex Zakotyanskyi <o.zakotianskyi@student.vu.nl>
2023-01-14 19:10:28 +0100

    feat: explicitly state port for flask add, add note on EXPOSE

--- docker-labs/flask-app/app.py
+++ docker-labs/flask-app/app.py
@@ -19,10 +19,12 @@ images = [
     "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr03/15/10/anigif_enhanced-buzz-11980-1381846269-1.gif"
 ]
 
+
 @app.route('/')
 def index():
     url = random.choice(images)
     return render_template('index.html', url=url)
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
